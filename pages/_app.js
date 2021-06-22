9b7682951fa2325fbe14c6aa83ee49c525ba0e41
defless <simon.deflesschouwer@mmibordeaux.com>
2021-06-22 18:36:42 +0200

    feat(ga): add pageview events

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,19 @@ import '../theme/styles.css';
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return <Component {...pageProps} />
 }
 
