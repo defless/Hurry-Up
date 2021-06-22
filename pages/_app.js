5c708ece582a44676ac4d07ad4a45f90f7135b3c
defless <simon.deflesschouwer@mmibordeaux.com>
2021-06-22 18:44:01 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ga from '../lib/ga'
 import '../theme/styles.css';
 import 'tailwindcss/tailwind.css'
 
